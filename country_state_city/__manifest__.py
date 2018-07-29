d162c8416e6c01514da0fdb6475ba023eae255bb
David Morrison <jdmorriso@gmail.com>
2018-07-29 11:07:42 +1000

    fix(city): add security records for city and allow non-admin to configure them

--- country_state_city/__manifest__.py
+++ country_state_city/__manifest__.py
@@ -28,7 +28,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
