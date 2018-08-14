82b0d0df1e4970be9c3540051c06501d0fc2a395
David Morrison <jdmorriso@gmail.com>
2018-08-14 13:20:29 +1000

    fix(delivery): ensure valid delivery days are from 1pm (ie min = 0)

--- website_tpp/vue/src/helpers/deliveryDays.js
+++ website_tpp/vue/src/helpers/deliveryDays.js
@@ -3,31 +3,31 @@ import moment from 'moment';
 
 const daily = {
     schedules: [ 
-        { d: [2,3,4,5,6], h: [13], },           // Weekdays: Mon, Tue, Wed, Thu, Fri at 1pm
-        { D: [14], M: [2], h: [13], },          // Valentines Day: 14th of February at 1pm
-        { dc: [2], d: [1], M: [3], h: [13], },  // Mothers Day: 2nd Sunday of March at 1pm
+        { d: [2,3,4,5,6], h: [13], m: [0] },           // Weekdays: Mon, Tue, Wed, Thu, Fri at 1pm
+        { D: [14], M: [2], h: [13], m: [0] },          // Valentines Day: 14th of February at 1pm
+        { dc: [2], d: [1], M: [3], h: [13], m: [0] },  // Mothers Day: 2nd Sunday of March at 1pm
     ],
     exceptions: [
-        { D: [25], M: [12], h: [13], },         // Christmas Day: 25th of December at 1pm
-        { D: [26], M: [12], h: [13], },         // Boxing Day: 26th of December at 1pm
-        { D: [1],  M: [1], h: [13], },          // New Years Day: 1st of January at 1pm
-        { D: [26], M: [1], h: [13], },          // Australia: 26th of January at 1pm
-        { D: [25], M: [4], h: [13], },          // Anzac Day: 25th of April at 1pm
-        { dc: [2], d: [2], M: [6], h: [13], },  // Queens Birthday: 2nd Monday of June at 1pm
-        { dc: [1], d: [2], M: [10], h: [13], }, // NSW Labour Day: 1st Monday of October at 1pm
+        { D: [25], M: [12], h: [13], m: [0] },         // Christmas Day: 25th of December at 1pm
+        { D: [26], M: [12], h: [13], m: [0] },         // Boxing Day: 26th of December at 1pm
+        { D: [1],  M: [1], h: [13], m: [0] },          // New Years Day: 1st of January at 1pm
+        { D: [26], M: [1], h: [13], m: [0] },          // Australia: 26th of January at 1pm
+        { D: [25], M: [4], h: [13], m: [0] },          // Anzac Day: 25th of April at 1pm
+        { dc: [2], d: [2], M: [6], h: [13], m: [0] },  // Queens Birthday: 2nd Monday of June at 1pm
+        { dc: [1], d: [2], M: [10], h: [13], m: [0] }, // NSW Labour Day: 1st Monday of October at 1pm
         // Easter Friday and Monday for the next 10 years
-        { D: [19,22], M: [4], Y: [2019], h: [13], },
-        { D: [10,13], M: [4], Y: [2020], h: [13], },
-        { D: [2,5],   M: [4], Y: [2021], h: [13], },
-        { D: [15,18], M: [4], Y: [2022], h: [13], },
-        { D: [7,10],  M: [4], Y: [2023], h: [13], },
-        { D: [29],    M: [3], Y: [2024], h: [13], }, { D: [1], M: [4], Y: [2024], h: [13], },
-        { D: [18,21], M: [4], Y: [2025], h: [13], },
-        { D: [3,6],   M: [4], Y: [2026], h: [13], },
-        { D: [26,29], M: [3], Y: [2027], h: [13], },
-        { D: [14,17], M: [4], Y: [2028], h: [13], },
-        { D: [30],    M: [3], Y: [2029], h: [13], }, { D: [2], M: [4], Y: [2029], h: [13], },
-        { D: [19,22], M: [4], Y: [2030], h: [13], },
+        { D: [19,22], M: [4], Y: [2019], h: [13], m: [0] },
+        { D: [10,13], M: [4], Y: [2020], h: [13], m: [0] },
+        { D: [2,5],   M: [4], Y: [2021], h: [13], m: [0] },
+        { D: [15,18], M: [4], Y: [2022], h: [13], m: [0] },
+        { D: [7,10],  M: [4], Y: [2023], h: [13], m: [0] },
+        { D: [29],    M: [3], Y: [2024], h: [13], m: [0] }, { D: [1], M: [4], Y: [2024], h: [13], m: [0] },
+        { D: [18,21], M: [4], Y: [2025], h: [13], m: [0] },
+        { D: [3,6],   M: [4], Y: [2026], h: [13], m: [0] },
+        { D: [26,29], M: [3], Y: [2027], h: [13], m: [0] },
+        { D: [14,17], M: [4], Y: [2028], h: [13], m: [0] },
+        { D: [30],    M: [3], Y: [2029], h: [13], m: [0] }, { D: [2], M: [4], Y: [2029], h: [13], m: [0] },
+        { D: [19,22], M: [4], Y: [2030], h: [13], m: [0] },
     ],
 };
 export function next90DeliveryDays() {
@@ -41,12 +41,12 @@ export default function deliveryDays(start, freq, number) {
     if (!startDate.isValid()) return '';
     const weekly = {
         schedules: [ 
-            { d: [startDate.day()+1], wm: [1,2,3,4,5,6], h: [13], },        // Starting day of week (2-6) for every Week of the month at 1pm
+            { d: [startDate.day()+1], wm: [1,2,3,4,5,6], h: [13], m: [0] },        // Starting day of week (2-6) for every Week of the month at 1pm
         ],
     };
     const monthly = {
         schedules: [ 
-            { D: [Math.min(startDate.date(),28)], M: [1,2,3,4,5,6,7,8,9,10,11,12], h: [13], },    // Starting day of month (1-28) for every Month of the year at 1pm
+            { D: [Math.min(startDate.date(),28)], M: [1,2,3,4,5,6,7,8,9,10,11,12], h: [13], m: [0] },    // Starting day of month (1-28) for every Month of the year at 1pm
         ],
     }
 
