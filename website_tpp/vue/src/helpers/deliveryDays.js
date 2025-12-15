701ad114cbde8d040aaade561b592665dd3a3f2a
David Morrison <jdmorriso@gmail.com>
2025-12-16 10:35:36 +1100

    enh(holidays): xmas 2025

--- website_tpp/vue/src/helpers/deliveryDays.js
+++ website_tpp/vue/src/helpers/deliveryDays.js
@@ -15,24 +15,22 @@ const daily = {
         { D: [13], M: [2], h: [13], m: [0] },          // Day before Valentines Day: 13th of February at ** 1pm **
         { D: [14], M: [2], h: [13], m: [0] },          // Valentines Day: 14th of February at ** 1pm **
         { dc: [2], d: [1], M: [5], h: [13], m: [0] },  // Mothers Day: 2nd Sunday of May at 1pm
-        { D: [22], M: [12], Y: [2024], h: [13], m: [0] },         // Xmas Sunday 22rd December 2024 at 1pm
     ],
     exceptions: [
         // { D: [4], M: [9], Y: [2021], h: [13], m: [0] },   // Shop closed day Sat 4-Sep-2021
         { D: [25], M: [12], h: [13], m: [0] },              // Christmas Day: 25th of December at 1pm
         { D: [26], M: [12], h: [13], m: [0] },              // Boxing Day: 26th of December at 1pm
-        // { D: [27], M: [12], h: [13], m: [0] },              // Post Xmas Break: 27th Dec
-        // { D: [27,28,29,30,31], M: [12], h: [13], m: [0] },         // Post Xmas Break: 27-31 Dec
+        { D: [27], M: [12], h: [13], m: [0] },              // Post Xmas Break: 27th Dec
+        // { D: [28,29,30,31], M: [12], h: [13], m: [0] },         // Post Xmas Break: 28-31 Dec
         { D: [1],  M: [1], h: [13], m: [0] },               // New Years Day: 1st of January at 1pm
         { D: [2],  M: [1], h: [13], m: [0] },               // Post New Years Day Break: 2nd of January at 1pm
-        { D: [3,4,6,7,8,9,10,11,13,14],  M: [1], Y: [2025], h: [13], m: [0] },       // Post New Years Day Break: 3-8 January at 1pm
+        { D: [3],  M: [1], h: [13], m: [0] },               // Post New Years Day Break: 3rd of January at 1pm
+        { D: [5,6,7,8,9,10],  M: [1], Y: [2026], h: [13], m: [0] },       // Post New Years Day Break
+        { D: [12,13,14,15,16,17],  M: [1], Y: [2026], h: [13], m: [0] },       // Post New Years Day Break
         { D: [26], M: [1], h: [13], m: [0] },               // Australia: 26th of January at 1pm
-        { D: [27], M: [1], Y: [2020], h: [13], m: [0] },    // Australia Day Monday Holiday
-        { D: [25,27], M: [1], Y: [2025], h: [13], m: [0] },    // Australia Day Monday Holiday 2025
         { D: [28], M: [1], Y: [2030], h: [13], m: [0] },    // Australia Day Monday Holiday
         { D: [27], M: [1], Y: [2031], h: [13], m: [0] },    // Australia Day Monday Holiday
         { D: [25], M: [4], h: [13], m: [0] },               // Anzac Day: 25th of April at 1pm
-        { D: [22], M: [9], Y: [2022], h: [13], m: [0] },    // Queens Day of Mourning 2022
         { dc: [2], d: [2], M: [6], h: [13], m: [0] },       // Queens Birthday: 2nd Monday of June at 1pm
         { dc: [1], d: [2], M: [10], h: [13], m: [0] },      // NSW Labour Day: 1st Monday of October at 1pm
         // Easter Friday and Monday for the next 10 years
